254aa05dbdac16d6ea6216ba0b54324b0f8a5996
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2023-04-28 16:51:40 -0600

    memento pattern

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import memento.Editor;
+import memento.History;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Editor editor = new Editor();
+        History history = new History();
+
+        editor.setContent("a");
+        history.push(editor.createState());
+
+        editor.setContent("b");
+        history.push(editor.createState());
+
+        editor.setContent("c");
+        editor.restore(history.pop());
+        editor.restore(history.pop());
+
+        System.out.println(editor.getContent());
     }
 }
---
+++ src/memento/Editor.java
@@ -0,0 +1,24 @@
+package memento;
+
+import java.util.ArrayList;
+
+public class Editor {
+    private String content;
+
+    public EditorState createState(){
+        return new EditorState(content);
+    }
+
+    public void restore(EditorState state){
+        content = state.getState();
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
