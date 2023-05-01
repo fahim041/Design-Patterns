d1d6507897fe306768cdb46377741c07037b2b1f
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2023-05-01 15:13:17 -0600

    observer design patterns

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import observer.Chart;
+import observer.DataSource;
+import observer.SpreadSheet;
+
 public class Main {
     public static void main(String[] args) {
-        String s = "Hello";
-        char[] c = s.toCharArray();
-        System.out.println(c.length);
+        var dataSource = new DataSource();
+        var sheet1 = new SpreadSheet();
+        var sheet2 = new SpreadSheet();
+        var chart = new Chart();
+
+        dataSource.addObserver(sheet1);
+        dataSource.addObserver(sheet2);
+        dataSource.addObserver(chart);
+
+        dataSource.setValue(1);
     }
 }
