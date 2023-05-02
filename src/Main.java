3f1913573645f84b277dbd7730454099d045e378
Fahim090 <46305384+M0nster92@users.noreply.github.com>
2023-05-02 12:50:43 -0600

    observer & chainOfResponsibility patterns

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,12 @@
-import observer.Chart;
-import observer.DataSource;
-import observer.SpreadSheet;
+import chainOfResponsibility.*;
 
 public class Main {
     public static void main(String[] args) {
-        var dataSource = new DataSource();
-        var sheet1 = new SpreadSheet();
-        var sheet2 = new SpreadSheet();
-        var chart = new Chart();
+        var compressor = new Compressor(null);
+        var logger = new Logger(compressor);
+        var auth = new Authenticator(logger);
 
-        dataSource.addObserver(sheet1);
-        dataSource.addObserver(sheet2);
-        dataSource.addObserver(chart);
-
-        dataSource.setValue(1);
+        var server = new WebServer(auth);
+        server.handle(new HttpRequest("admin", "1234"));
     }
 }
---
+++ src/chainOfResponsibility/Logger.java
@@ -0,0 +1,13 @@
+package chainOfResponsibility;
+
+public class Logger extends Handler {
+    public Logger(Handler next) {
+        super(next);
+    }
+
+    @Override
+    public boolean doHandle(HttpRequest httpRequest) {
+        System.out.println("logger");
+        return false;
+    }
+}
